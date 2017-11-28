cd6a488ee587e70ff8185cca7222d60a7292bdf2
Shaun Wahle <shaun.wahle@gmail.com>
2017-11-28 12:00:59 -0800

    finished with this project for today

---
+++ src/reducer/user.js
@@ -0,0 +1,14 @@
+let { type, payload } = action;
+switch (type) {
+    case 'USER_SET':
+        return payload;
+    case 'USER_CREATE':
+        return [payload, ...state];
+    case 'USER_UPDATE':
+        // return state.map(item => item.userID === payload.userID ? payload : item);
+        return payload;
+    case 'USER_DELETE':
+        return state.filter(item => item.userID !== payload.userID);
+    default: return state;
+}
+};
